8befb73c6d200bba5f71d6706b1ba2c8dc5baf24
Nagaraju Bandla <101219231+kavithakukkapalli@users.noreply.github.com>
2022-03-22 09:01:26 +0000

    added seed-data

--- Src/PatientDashBoard/PatientDashBoard.Database/Migrations/Configuration.cs
+++ Src/PatientDashBoard/PatientDashBoard.Database/Migrations/Configuration.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity.Migrations;
+﻿using System;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using PatientDashBoard.Models;
 
 namespace PatientDashBoard.Database.Migrations
 {
@@ -12,12 +15,62 @@ namespace PatientDashBoard.Database.Migrations
         protected override void Seed(PatientDBContext context)
         {
 
-            
+            if (!context.Patient.Any())
+            {
+                Patient c = new Patient
+                {
+                    PatientId = 1,
+                    FirstName = "Kavitha",
+                    LastName = "Kukkapalli",
+                    EMail = "test@gmail.com",
+                    Deleted = false,
+                    Dob = DateTime.Parse("12/05/1990"),
+                    NhsNumber = "123454667",
+                    PhoneNumber = "075065646545",
+                    Status = "Active"
+                };
+                var b = new Patient
+                {
+                    PatientId = 1,
+                    FirstName = "Dean",
+                    LastName = "Garetty",
+                    EMail = "dean@gmail.com",
+                    Deleted = false,
+                    Dob = DateTime.Parse("12/05/1980"),
+                    NhsNumber = "1234343242347",
+                    PhoneNumber = "075063453455",
+                    Status = "Active"
+                };
+                var a = new Patient
+                {
+                    PatientId = 1,
+                    FirstName = "Stephen",
+                    LastName = "Frackelton",
+                    EMail = "stephen@gmail.com",
+                    Deleted = false,
+                    Dob = DateTime.Parse("12/05/1980"),
+                    NhsNumber = "5764564",
+                    PhoneNumber = "0755645645",
+                    Status = "Active"
+                };
 
-            //  This method will be called after migrating to the latest version.
+                var cAddress = new Address
+                {
+                    PatientId = 1,
+                    AddressLine1 = "12",
+                    AddressLine2 = "Some street",
+                    PostCode = "L11 1AP",
+                    City = "Liverpool",
+                    County = "Mersyside"
+                };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+                context.Patient.Add(a);
+                context.Patient.Add(b);
+                context.Patient.Add(c);
+
+                context.PatientAddress.Add(cAddress);
+                context.SaveChanges();
+            }
         }
     }
 }
